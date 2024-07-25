8fd63cf3b28a1e179bc1140d7d0817fc4e9cb591
tunlyinaung0 <tunlyinaung@gmail.com>
2024-07-26 01:35:09 +0630

    add s3 backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.59.0"
     }
   }
+
+  backend "s3" {
+    bucket = "tf-sandbox-assignment"
+    key    = "terraform.tfstate"
+    region = local.region
+  }
 }
 
 provider "aws" {
